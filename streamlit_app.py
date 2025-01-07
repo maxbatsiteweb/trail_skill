76013bf01bb8da3b8510968e66a1e6f839114933
maxbatsiteweb <73015150+maxbatsiteweb@users.noreply.github.com>
2025-01-07 18:11:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ from streamlit_echarts import st_echarts  # Pour le donut
 import pandas as pd
 import plotly.graph_objects as go
 
-st.set_option('server.headless', True)  # Assurez-vous que l'application est en mode headless (utile en cloud)
-st.set_option('server.enableCORS', False)  # Si vous avez des problèmes avec les CORS
+
 
 
 st.image("logo_esc.png", width=150)
